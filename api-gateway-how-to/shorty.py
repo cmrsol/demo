1e25fceea255aa7c0edec549b9d82f2cd843bbd8
Duke P. Takle <duke.takle@cmrsol.com>
2016-10-30 22:18:17 -0500

    Clean

--- api-gateway-how-to/shorty.py
+++ api-gateway-how-to/shorty.py
@@ -3,7 +3,7 @@ import boto3
 db = boto3.client('dynamodb')
 
 def request_handler(event, context):
-    print("request_handler(): called")
+    print('request_handler(): called')
     print(str(event))
     print(str(context))
 
@@ -13,12 +13,12 @@ def request_handler(event, context):
         else:
             return write_entry(event)
     except Exception as x:
-        print("exception: " + str(x))
+        print('exception: ' + str(x))
         return
 
 
 def write_entry(event):
-    print("write_entries(): called")
+    print('write_entries(): called')
     i = {}
     i['url_name'] = {'S' : event['url_name'] }
     i['url'] = {'S' : event['url'] }
@@ -32,17 +32,17 @@ def write_entry(event):
 
 
 def read_entry(event):
-    print("read_entries(): called")
+    print('read_entries(): called')
     i = {}
     i['url_name'] = {'S' : event['url_name'] }
     print(i)
     try:
         items = db.get_item(TableName = 'urly', Key=i )
-        print("items:")
+        print('items:')
         print(str(items))
         if 'Item' in items:
             url = items['Item']['url']['S']
-            print("found: " + str(url))
+            print('found: ' + str(url))
             return { 'location' : url}
         else:
             return { 'location' : 'http://www.amazon.com?msg=not_found'}
@@ -52,6 +52,6 @@ def read_entry(event):
         return { 'location' : 'http://www.amazon.com?msg=bad'}
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     event = {}
     context = {}
