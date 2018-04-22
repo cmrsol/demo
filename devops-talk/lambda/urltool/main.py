80ddd857f29e9f655f2697a687835c4f561adfe6
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2018-04-22 14:26:08 -0500

    wip

--- devops-talk/lambda/urltool/main.py
+++ devops-talk/lambda/urltool/main.py
@@ -1,13 +1,17 @@
 from utility import FlaskLambda
 from flask import request
-import json
+from index import index_page
 import os
+import json
+import uuid
+import boto3
 
 '''
 The FlaskLambda object that is created is the entry point for the lambda. The
 LambdaTool deployer expects this to be called 'lambda_handler'
 '''
 lambda_handler = FlaskLambda(__name__)
+s3_client = boto3.client('s3')
 
 
 @lambda_handler.route('/', methods=['GET'])
@@ -22,32 +26,14 @@ def document():
         tuple of (body, status code, content type) that API Gateway understands
     '''
     return (
-        slash_html,
+        index_page,
         200,
         {'Content-Type': 'text/html'}
     )
 
 
-@lambda_handler.route('/answer', methods=['GET'])
-def get_answer():
-    '''
-    Example of getting someething from function.properties
-
-    Args:
-        None
-
-    Returns:
-        tuple of (body, status code, content type) that API Gateway understands
-    '''
-    return (
-        os.environ.get('ANSWER', '0'),
-        200,
-        {'Content-Type': 'text/html'}
-    )
-
-
-@lambda_handler.route('/food', methods=['GET', 'POST'])
-def food():
+@lambda_handler.route('/url', methods=['POST'])
+def handle_url():
     '''
     A contrived example function that will return some meta-data about the
     invocation.
@@ -58,11 +44,19 @@ def food():
     Returns:
         tuple of (body, status code, content type) that API Gateway understands
     '''
+    print('handle_url() called')
+    key = str(uuid.uuid4())[:8]
+    req = request.json
+    url = req.get('url', None)
+    store_shortened(key, url)
     data = {
-        'form': request.form.copy(),
-        'args': request.args.copy(),
-        'json': request.json
+        'rtype': str(type(req)),
+        'keys': str(req.keys()),
+        'key': key,
+        'url': req.get('url', 'unknown')
     }
+
+    print('handle_url() data: {}'.format(data))
     return (
         json.dumps(data, indent=4, sort_keys=True),
         200,
@@ -70,13 +64,28 @@ def food():
     )
 
 
+def store_shortened(key, url):
+    try:
+        s3_client.put_object(
+            ACL='private',
+            Bucket=os.environ.get('bucket', None),
+            Body=url,
+            Key=key
+        )
+
+        return True
+    except Exception as wtf:
+        print('store_shortened() explodeded: {}'.format(wtf))
+
+    return False
+
 if __name__ == '__main__':
     lambda_handler.run(debug=True)
 
-slash_html = '''<html xmlns="http://www.w3.org/1999/xhtml">
+jump_html = '''<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <title>Lambtool Readme</title>
-    <meta http-equiv="refresh" content="0;URL='https://github.com/muckamuck/lambda-tool/blob/master/README.md'" />
+    <meta http-equiv="refresh" content="0;URL='{}'" />
   </head>
   <body></body>
 </html>
