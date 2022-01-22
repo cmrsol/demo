0149c5cf958de7ccf2654d668ba9fe061803fbb6
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2022-01-22 14:58:32 +0000

    catch up

--- devops-talk/demo.sh
+++ devops-talk/demo.sh
@@ -3,25 +3,19 @@
 cd $(dirname ${0})
 set -e
 
-echo "================================================================================" | tee output.txt
+echo "== STARTING THE VIRTUAL ENVIROMENT ============================================="
 food=$(date +%s); virtualenv /tmp/${food}; . /tmp/${food}/bin/activate
+pip install -Ur requirements.txt
+echo "=== STARTING THE VPC ==========================================================="
+time (cd vpc; stackility upsert -i config.ini)
+echo "=== STARTING THE IAM ROLES ====================================================="
+time (cd iam; stackility upsert -i config.ini)
+echo "=== STARTING THE EC2 MACHINE ==================================================="
+time (cd ec2; stackility upsert -i config.ini)
+echo "=== STARTING THE APPLICATION SECURITY GROUP ===================================="
+time (cd sg; stackility upsert -i config.ini)
+echo "=== STARTING THE S3 BUCKET TO HOLD THE DATA ===================================="
+time (cd s3; stackility upsert -i config.ini)
+echo "=== STARTING THE API GATEWAY / LAMBDA FUNCTION ================================="
+time (cd lambda/urltool; lambdatool deploy -s demo -r us-east-2 --profile demo)
 echo "================================================================================"
-
-pip install -Ur requirements.txt | tee -a output.txt
-time (cd vpc; stackility upsert -i config.ini) | tee -a output.txt
-echo "================================================================================" | tee -a output.txt
-
-time (cd iam; stackility upsert -i config.ini) | tee -a output.txt
-echo "================================================================================" | tee -a output.txt
-
-time (cd ec2; stackility upsert -i config.ini) | tee -a output.txt
-echo "================================================================================" | tee -a output.txt
-
-time (cd sg; stackility upsert -i config.ini) | tee -a output.txt
-echo "================================================================================" | tee -a output.txt
-
-time (cd s3; stackility upsert -i config.ini) | tee -a output.txt
-echo "================================================================================" | tee -a output.txt
-
-time (cd lambda/urltool; lambdatool deploy -s demo -r us-east-2 --profile demo) | tee -a output.txt
-echo "================================================================================" | tee -a output.txt
